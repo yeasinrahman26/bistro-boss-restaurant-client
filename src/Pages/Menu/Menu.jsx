25fb6909f995b17b78569ee43637a0df5471642e
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2025-01-19 04:33:36 +0600

    menu section complete

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -6,10 +6,20 @@ import menuImg1 from '../../assets/menu/pizza-bg.jpg'
 import menuImg2 from '../../assets/menu/salad-bg.jpg'
 import menuImg3 from '../../assets/menu/soup-bg.jpg'
 import menuImg4 from '../../assets/menu/dessert-bg.jpeg'
+import useMenu from "../../hooks/UseMenu";
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import MenuCategory from "./MenuCategory/MenuCategory";
 
 
 
 const Menu = () => {
+  const [menu]=useMenu();
+  const desserts=menu.filter(item=> item.category === "dessert") ;
+  const pizza=menu.filter(item=> item.category === "pizza") ;
+  const salad=menu.filter(item=> item.category === "salad") ;
+  const soup=menu.filter(item=> item.category === "soup") ;
+  const offered=menu.filter(item=> item.category === "offered") ;
+  console.log(desserts);
     return (
       <div>
         <Helmet>
@@ -21,35 +31,42 @@ const Menu = () => {
           header={"OUR MENU"}
           paragraph={"Would you like to try a dish?"}
         ></Cover>
-        <PopularMenu></PopularMenu>
+        <SectionTitle
+          subHeading={"Don`t Miss "}
+          heading={"Today`s Offer"}
+        ></SectionTitle>
+        <MenuCategory items={offered}></MenuCategory>
+
         {/*  */}
         <Cover
           coverImg={menuImg1}
           header={"PIZZA"}
           paragraph={"Would you like to try a dish?"}
         ></Cover>
-        <PopularMenu></PopularMenu>
+        <MenuCategory items={pizza}></MenuCategory>
+
         {/*  */}
         <Cover
           coverImg={menuImg2}
-          header={"SALAd"}
+          header={"SALAD"}
           paragraph={"Would you like to try a dish?"}
         ></Cover>
-        <PopularMenu></PopularMenu>
+        <MenuCategory items={salad}></MenuCategory>
         {/*  */}
         <Cover
           coverImg={menuImg3}
           header={"SOUP"}
           paragraph={"Would you like to try a dish?"}
         ></Cover>
-        <PopularMenu></PopularMenu>
+        <MenuCategory items={soup}></MenuCategory>
+
         {/*  */}
         <Cover
           coverImg={menuImg4}
           header={"DESERT"}
           paragraph={"Would you like to try a dish?"}
         ></Cover>
-        <PopularMenu></PopularMenu>
+        <MenuCategory items={desserts}></MenuCategory>
       </div>
     );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,10 +1,15 @@
+import MenuItemCard from "../../Shared/MenuItemCard/MenuItemCard";
 
 
-const MenuCategory = () => {
+const MenuCategory = ({items}) => {
     return (
-        <div>
-            
+      <div>
+        <div className="grid mx-4 my-10 md:mx-0 md:grid-cols-2 gap-10">
+          {items.map((item) => (
+            <MenuItemCard key={item._id} item={item}></MenuItemCard>
+          ))}
         </div>
+      </div>
     );
 };
 
